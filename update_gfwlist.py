67b46a22d05bd84fb0b3ff673f82c731dabef96b
SabeiOth <samoro@qq.com>
2024-11-07 13:38:37 +0800

    Update update_gfwlist.py

--- update_gfwlist.py
+++ update_gfwlist.py
@@ -2,70 +2,12 @@ import requests
 import base64
 import re
 import os
-from collections import defaultdict
 
 # 配置
 DNS_FORWARD_IP = "198.18.0.1"
 GFWLIST_URL = "https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
 OUTPUT_FILE = "dns.rsc"
 GFWLIST_RSC_FILE = "gfwlist.rsc"
-
-# 定义公司及其对应的正则表达式
-COMPANY_REGEX_MAP = {
-    "Google": [
-        r"(youtube|ytimg|android|androidify|appspot|autodraw|blogger|capitalg|chrome|chromeexperiments|chromestatus|creativelab5|debug|deepmind|dialogflow|firebaseio|googletagmanager|ggpht|gmail|gmodules|gstatic|gv|gvt0|gvt1|gvt2|gvt3|itasoftware|madewithcode|synergyse|tiltbrush|waymo|ampproject|certificate-transparency|chromium|getoutline|godoc|golang|gwtproject|polymer-project|tensorflow|waveprotocol|webmproject|webrtc|whatbrowser|material|shattered|recaptcha|abc|admin|getmdl)\.(com|net|org|xyz|io)$",
-        r"google\.[a-z]+\.[a-z]+$",  # 匹配所有 google 子域名
-        r"google\.[a-z]+$"           # 匹配所有 google 顶级域名
-    ],
-    "Microsoft": [
-        r"(azure|bing|live|outlook|msn|surface|1drv|microsoft|azureedge|msauth|[a-z]-msedge|cd20|office|microsoftonline|msecnd|msftauth|skype|onedrive|modpim)\.(com|net|org)$"
-    ],
-    "Facebook": [
-        r"(messenger|whatsapp|oculus|oculuscdn|cdninstagram|fb|fbcdn|instagram)\.(com|net|me)$"
-    ],
-    "Twitter": [
-        r"(twimg|twitpic|twitter)\.(co|com)$"
-    ],
-    "Apple": [
-        r"(icloud|me|appsto|appstore|aaplimg|crashlytics|mzstatic)\.(com|co|re)$"
-    ],
-    "Amazon": [
-        r"(amazon|amazonaws|kindle|primevideo)\.com$"
-    ],
-    "Netflix": [
-        r"(nflx|netflix|fast)\.(net|com)$"
-    ],
-    "Spotify": [
-        r"(spotify|tidal|pcdn|scdn|pscdn)\.(com|co)$"
-    ],
-    "Telegram": [
-        r"telegram\.org$"
-    ],
-    "Other": [
-        r"(dropbox|hbo|quora|quoracdn|yahoo|ytimg|scorecardresearch|dazn|happyon|itv|itvstatic|pandora|pbs|4gtv|kktv|kfs|kkbox|twitch|ttvnw|viu|my5|channel5|litv|boltdns|encoretvb|mytvsuper|tvb|joox|deezer|dzcdn|bbc|bbci|c4assets|channel4|abema|ameba|hayabusa|fdcservers|yoshis|extride|chinaunicomglobal|ipinfo|ip|edge\.api\.brightcove|videos-f\.jwpsrv|content\.jwplatform)\.(com|net|org|io|tv|jp|co\.uk|me|re|sb)$"
-    ]
-}
-
-# 定义已知国内可访问的域名列表
-DOMESTIC_DOMAINS = [
-    "aliyun.com",
-    "baidu.com",
-    "chinaso.com",
-    "chinaz.com",
-    "haosou.com",
-    "ip.cn",
-    "jike.com",
-    "gov.cn",
-    "qq.com",
-    "sina.cn",
-    "sogou.com",
-    "so.com",
-    "soso.com",
-    "weibo.com",
-    "yahoo.cn",
-    "youdao.com",
-    "zhongsou.com"
-]
 
 def fetch_gfwlist(url):
     response = requests.get(url)
@@ -78,57 +20,42 @@ def save_base64_gfwlist(gfwlist, filename):
     with open(filename, "wb") as f:
         f.write(base64.b64decode(gfwlist))
 
-def process_gfwlist(gfwlist):
-    processed_rules = ["/ip dns static"]
-    company_domains = {company: [] for company in COMPANY_REGEX_MAP.keys()}
-    unmatched_domains = []
+def extract_general_list(gfwlist):
+    start_marker = "!##############General List Start###############"
+    end_marker = "!##############General List End#################"
+    start_index = gfwlist.find(start_marker)
+    end_index = gfwlist.find(end_marker)
 
-    # 存储已处理的域名以去重
-    seen_domains = set()
+    if start_index == -1 or end_index == -1:
+        raise Exception("Markers not found in the GFW list")
 
-    for line in gfwlist.splitlines():
-        if line.startswith('!') or not line.strip():
-            continue
-        
-        domain = line[2:].strip('^') if line.startswith('||') else line
-        if domain in seen_domains or any(domain.endswith(f".{domestic_domain}") for domestic_domain in DOMESTIC_DOMAINS):
-            continue
+    return gfwlist[start_index + len(start_marker):end_index].strip()
 
-        # 处理子域名合并
-        parts = domain.split('.')
-        if len(parts) > 2:
-            parent_domain = '.'.join(parts[-2:])
-            if parent_domain in seen_domains or any(parent_domain.endswith(f".{domestic_domain}") for domestic_domain in DOMESTIC_DOMAINS):
-                continue
-            domain = parent_domain
+def parse_groups(gfwlist):
+    groups = {}
+    current_group = None
+    lines = gfwlist.splitlines()
 
-        seen_domains.add(domain)
+    for line in lines:
+        if line.startswith("!!---"):
+            group_name = line[5:].strip()  # Remove leading "!---" and trailing spaces
+            current_group = group_name
+            groups[current_group] = []
+        elif current_group is not None and line.strip():
+            domain = line.strip().lstrip('|').strip('/')  # Remove leading '||' and trailing '/'
+            if domain and not domain.startswith("!") and not domain.startswith("@@"):
+                groups[current_group].append(domain)
 
-        matched_company = None
-        for company, regex_list in COMPANY_REGEX_MAP.items():
-            for regex in regex_list:
-                if re.match(regex, domain):
-                    matched_company = company
-                    break
-            if matched_company:
-                break
-        
-        if matched_company:
-            company_domains[matched_company].append(domain)
-        else:
-            unmatched_domains.append(domain)
-    
-    for company, domains in company_domains.items():
+    return groups
+
+def generate_dns_rules(groups):
+    rules = ["/ip dns static"]
+    for group, domains in groups.items():
         if domains:
             combined_regex = "|".join(re.escape(domain) for domain in domains)
-            company_rule = f'add comment={company} forward-to={DNS_FORWARD_IP} regexp="^({combined_regex})$" type=FWD'
-            processed_rules.append(company_rule)
-    
-    for domain in unmatched_domains:
-        rule = f'add comment=GFW forward-to={DNS_FORWARD_IP} regexp="^{domain}$" type=FWD"'
-        processed_rules.append(rule)
-    
-    return "\n".join(processed_rules)
+            rule = f'add comment="{group}" forward-to={DNS_FORWARD_IP} regexp="^({combined_regex})$" type=FWD'
+            rules.append(rule)
+    return "\n".join(rules)
 
 def save_rules(rules, filename):
     with open(filename, "w") as f:
@@ -142,9 +69,18 @@ def main():
         save_base64_gfwlist(gfwlist, GFWLIST_RSC_FILE)
         print(f"GFW list saved as decoded file: {GFWLIST_RSC_FILE}")
 
-        processed_rules = process_gfwlist(gfwlist)
-        save_rules(processed_rules, OUTPUT_FILE)
-        print(f"Rules saved to {OUTPUT_FILE}")
+        # 提取 General List 范围内的内容
+        general_list = extract_general_list(gfwlist)
+
+        # 解析分组信息
+        groups = parse_groups(general_list)
+
+        # 生成 DNS 规则
+        dns_rules = generate_dns_rules(groups)
+
+        # 保存 DNS 规则到 dns.rsc 文件
+        save_rules(dns_rules, OUTPUT_FILE)
+        print(f"DNS rules saved to {OUTPUT_FILE}")
     except Exception as e:
         print(f"Error: {e}")
 
