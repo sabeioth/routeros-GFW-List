d3dd6199bcaf5ba0232466143579d5020920d187
SabeiOth <samoro@qq.com>
2024-11-07 22:22:00 +0800

    Update update_gfwlist.py

--- update_gfwlist.py
+++ update_gfwlist.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 # 下载 gfw.txt 文件
 url = 'https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/gfw.txt'
@@ -16,8 +17,10 @@ if response.status_code == 200:
     with open('dns.rsc', 'w') as f:
         for domain in domains:
             if domain and not domain.startswith('#'):  # 跳过注释行和空行
+                # 对域名进行正则表达式转义
+                escaped_domain = re.escape(domain)
                 # 写入 RouterOS 兼容的命令
-                f.write(f"/ip dns static add regexp=\"^{domain}$\" type=FWD address=198.18.0.1\n")
+                f.write(f"/ip dns static add regexp=\"^{escaped_domain}$\" type=FWD address=198.18.0.1\n")
     
     print("gfwlist.rsc 和 dns.rsc 文件已创建。")
 else:
