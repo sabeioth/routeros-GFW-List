d80215f4de4b199e8f32d0c60bb196478bd27e5a
SabeiOth <samoro@qq.com>
2024-11-07 18:28:52 +0800

    Update update_gfwlist.py

--- update_gfwlist.py
+++ update_gfwlist.py
@@ -1,7 +1,6 @@
 import base64
 import requests
 import logging
-import re
 
 # 设置日志
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -36,43 +35,30 @@ def process_gfwlist(decoded_content):
             lines.append(line)
     return lines
 
-# 合并特定国家顶级域名条目
-def merge_specific_tlds(processed_lines, tlds):
-    tld_regex = '|'.join(map(re.escape, tlds))
-    tld_pattern = re.compile(rf'\.(?:{tld_regex})$')
-    merged_lines = []
-    specific_tld_lines = []
-
-    for line in processed_lines:
-        if tld_pattern.search(line):
-            specific_tld_lines.append(line)
-        else:
-            merged_lines.append(line)
-
-    if specific_tld_lines:
-        merged_lines.append(f'(?:{tld_regex})$')
-    
-    return merged_lines, specific_tld_lines
+# 生成特定国家顶级域名的正则表达式条目
+def generate_tld_rules(tlds):
+    rules = []
+    for tld in tlds:
+        rule = f'\\.{tld}$'
+        rules.append((rule, f"Country {tld}"))
+    return rules
 
 # 生成 .rsc 文件内容
-def generate_rsc_content(merged_lines, specific_tld_lines):
+def generate_rsc_content(processed_lines, tld_rules):
     rsc_content = "/ip dns static\n"
     
     def add_rule(comment, domain_pattern):
         nonlocal rsc_content
         rsc_content += f'add comment="{comment}" forward-to=198.18.0.1 regexp="{domain_pattern}" type=FWD\n'
     
-    for line in merged_lines:
-        if line in specific_tld_lines:
-            # 如果是特定 TLD 的条目，添加特定的注释
-            tld = line.split('.')[-1]
-            comment = f"Country {tld}"
-        else:
-            comment = "GFW"
-        
-        # 假设每行都是一个需要添加到 DNS 静态记录中的域名模式
-        domain_pattern = line.replace('.', '\\.').replace('*', '.*')
+    # 添加特定国家顶级域名的规则
+    for domain_pattern, comment in tld_rules:
         add_rule(comment, domain_pattern)
+    
+    # 添加其他规则
+    for line in processed_lines:
+        domain_pattern = line.replace('.', '\\.').replace('*', '.*')
+        add_rule("GFW", domain_pattern)
     
     return rsc_content
 
@@ -93,12 +79,12 @@ def main():
         # 处理 gfwlist 内容
         processed_lines = process_gfwlist(decoded_content)
         
-        # 合并特定国家顶级域名条目
+        # 生成特定国家顶级域名的规则
         tlds = ['cu', 'at', 'ca', 'nz', 'br', 'jp', 'in', 'tw', 'hk', 'mo', 'ph', 'vn', 'tr', 'my', 'sg', 'it', 'uk', 'us', 'kr', 'ru', 'fr', 'de', 'ms', 'be', 'fi']
-        merged_lines, specific_tld_lines = merge_specific_tlds(processed_lines, tlds)
+        tld_rules = generate_tld_rules(tlds)
         
         # 生成并保存转换后的 dns.rsc 文件
-        rsc_content = generate_rsc_content(merged_lines, specific_tld_lines)
+        rsc_content = generate_rsc_content(processed_lines, tld_rules)
         write_rsc_file(rsc_content, 'dns.rsc')
         
         logging.info("Conversion complete, generated gfwlist.rsc and dns.rsc files.")
